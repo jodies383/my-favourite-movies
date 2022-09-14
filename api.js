74eb3f45281e402ac3f1db78ac3f5c8b91a77b09
Jodie Solomons <jodies383@gmail.com>
2022-09-14 11:47:14 +0200

    commit

---
+++ api.js
@@ -0,0 +1,178 @@
+module.exports = function (app, db) {
+    const jwt = require('jsonwebtoken')
+    const bcrypt = require('bcrypt');
+    const saltRounds = 10;
+
+    app.get('/api/test', function (req, res) {
+		res.json({
+			name: 'joe'
+		});
+	});
+    
+    function verifyToken(req, res, next) {
+        const token = req.headers.authorization && req.headers.authorization.split(" ")[1];
+        if (!req.headers.authorization || !token) {
+            res.sendStatus(401);
+            return;
+        }
+        try {
+            const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+            const { username } = decoded;
+
+            if (username) {
+                next();
+            } else {
+                res.status(403).json({
+                    message: 'unauthorized'
+                });
+            }
+        } catch (err) {
+            if (err && 500) {
+                res.json({
+                    message: 'expired'
+                })
+            }
+            next()
+
+
+        }
+
+    }
+
+    app.post('/api/register', async function (req, res, next) {
+        try {
+            const { username } = req.body;
+            const { password } = req.body;
+            const { firstName } = req.body
+            const { lastName } = req.body
+   
+            let checkDuplicate = await db.manyOrNone(`SELECT * from users WHERE username = $1`, [username]);
+            bcrypt.genSalt(saltRounds, async function (err, salt) {
+                bcrypt.hash(password, salt, async function (err, hash) {
+                    // Store hash in your password DB.
+                    if (checkDuplicate.length < 1) {
+                        await db.none(`insert into users (username, password, first_name, last_name) values ($1, $2, $3, $4)`, [username, hash, firstName, lastName])
+                        res.json({
+                            message: 'success'
+                        });
+                    } else {
+                        res.json({
+                            message: 'duplicate'
+                        });
+                    }
+                });
+            });
+        } catch (err) {
+            console.log(err);
+            next()
+        }
+
+    })
+    app.post('/api/login', async function (req, res, next) {
+        try {
+            const { username } = req.body;
+            const { password } = req.body;
+            const token = jwt.sign({
+                username
+            }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '4hr' });
+            let checkUser = await db.manyOrNone(`SELECT id from users WHERE username = $1`, [username]);
+            if (checkUser.length < 1) {
+                res.json({
+                    token,
+                    message: 'unregistered'
+                });
+            } else {
+
+                let checkPassword = await db.oneOrNone(`SELECT password from users WHERE username = $1`, [username]);
+
+                const match = await bcrypt.compare(password, checkPassword.password);
+
+                if (match) {
+                    res.json({
+                        token,
+                        message: 'success'
+                    });
+                } else {
+                    res.json({
+                        token,
+                        message: 'unmatched'
+                    });
+                }
+            }
+        } catch (err) {
+            console.log(err);
+            next()
+        }
+    });
+
+    app.get('/api/playlist/:username', verifyToken, async function (req, res, next) {
+        try {
+            const username = req.params.username
+           
+
+                const { id } = await db.oneOrNone(`select id from users where username = $1`, [username])
+                const userInfo = await db.manyOrNone(`select * from users where username = $1`, [username])
+                const playlist = await db.manyOrNone(`SELECT * from playlist WHERE user_id = $1`, [id]);
+                res.json({
+                    user: userInfo,
+                    playlist
+                })
+            
+
+        } catch (err) {
+            console.log(err);
+            next()
+        }
+    });
+
+    app.post('/api/playlist/:username', verifyToken, async function (req, res, next) {
+        try {
+            const username = req.params.username
+            const {movieId} = req.body
+      
+         
+            const { id } = await db.oneOrNone(`SELECT id from users WHERE username = $1`, [username]);
+           
+            let checkUser = await db.manyOrNone(`SELECT * from playlist WHERE user_id = $1 and movie_id = $2`, [id, movieId]);
+            if (checkUser.length < 1) {
+                await db.none(`insert into playlist (movie_id, user_id) values ($1, $2)`, [movieId, id])
+
+                res.json({
+                    message: 'success'
+                });
+            } else {
+                res.json({
+                    message: 'duplicate'
+                });
+            }
+        } catch (err) {
+            console.log(err);
+            next()
+        }
+    });
+    app.delete('/api/playlist',verifyToken, async function (req, res, next) {
+        try {
+
+            const username = req.query.username
+            const movieId = req.query.movie_id
+
+            try {
+                const { id } = await db.one(`select id from users where username = $1`, [username])
+                await db.none(`delete from playlist WHERE user_id = $1 and movie_id = $2`, [id, movieId]);
+                res.json({
+                    status: 'success'
+                })
+            } catch (err) {
+                res.json({
+                    status: 'success',
+                    error: err.stack
+                })
+            }
+        } catch (err) {
+            console.log(err);
+            next()
+        }
+    });
+
+
+}
---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const PgPromise = require('pg-promise');
+const jwt = require('jsonwebtoken');
+require('dotenv').config()
+
+const initOptions = {/* initialization options */};
+const pgp = PgPromise(initOptions);
+
+const DATABASE_URL= process.env.DATABASE_URL || "postgresql://jodie@localhost:5432/movie_api";
+
+const config = { 
+    connectionString : DATABASE_URL
+}
+
+if (process.env.NODE_ENV == 'production') {
+    config.ssl = { 
+        rejectUnauthorized : false
+	}
+}
+
+const db = pgp(config);
+
+// enable the req.body object - to allow us to use HTML forms
+// and when using POST requests
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+const API = require('./api');
+API(app, db);
+
+//configure the port number using and environment number
+var portNumber = process.env.PORT || 4000;
+
+//start everything up
+app.listen(portNumber, async function () {
+    console.log('server listening on:', portNumber);
+    async function testConnection() {
+        const c = await db.connect(); // try to connect
+        c.done(); // success, release connection
+        return c.client.serverVersion; // return server version
+    }
+    await testConnection()
+});
